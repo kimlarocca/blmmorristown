d42ad633e1a1d7e632b1e5ee40b2f95cace68a42
Kim LaRocca <kim@kimlarocca.com>
2020-06-19 15:09:54 -0400

    updated resources copy

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -7,7 +7,14 @@
       <div class="l-grid l-grid--4x1 l-grid--1up--small l-grid--large-gutters">
         <div>
           <h2 class="u-space--bottom">Coming Soon</h2>
-          <p>Local resources are coming soon!<br><br>In the meantime, <a href="https://blacklivesmatter.com/resources/" target="_blank">click here to visit Black Lives Matter's resources page</a>.</p>
+          <p>Local resources are coming soon!</p>
+          <br><br>
+          <p>In the meantime, here are a few links to check out:</p>
+          <br>
+          <ul>
+            <li><a href="https://www.d4pa.org" target="_blank">database for police abolition</a></li>
+            <li><a href="https://blacklivesmatter.com/resources/" target="_blank">black lives matter resource page</a></li>
+          </ul>
         </div>
         <sidebar/>
       </div>
