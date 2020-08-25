11806b6486f887b7f4a4f5bcf406d4b9b70f3436
mari <mari@ionlearning.com>
2020-08-25 16:20:21 -0400

    local and national news

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,15 +31,18 @@
     <section class="l-container">
       <h2 class="u-space--double--bottom u-align--center">Local News</h2>
       <div class="l-grid l-grid--3up">
-        <card
-          image="https://blacklivesmatter.com/wp-content/uploads/2020/06/blmgn-news-juneteenth-national-holiday-061720-1000x750-01-400x300.jpg"
-          altText="Make Juneteenth a National Holiday"
-          subtitle="Make Juneteenth a National Holiday"
-          :hasTextLink="true"
-          cta="Read More"
-          link="https://blacklivesmatter.com/make-juneteenth-a-national-holiday/"
-          description="Now more than ever, we must highlight and celebrate the rich history of our community and our culture. In the last few days, cities and companies across the country have declared Juneteenth a holiday. Just like our lives, our HISTORY matters, including why Juneteenth is rooted in joy and is a call for liberation for the Black community."
-        />
+        <template v-for="article in local_news">
+          <card
+            :key="article._uid"
+            :image="article.image.filename"
+            :altText="article.Title"
+            :subtitle="article.Title"
+            :hasTextLink="true"
+            cta="Read More"
+            :link="article.link.url"
+            :description="article.Blurb"
+          />
+        </template>
       </div>
     </section>
     <v-spacer size="quin"/>
@@ -50,31 +53,18 @@
     <section class="l-container">
       <h2 class="u-space--double--bottom u-align--center">National News</h2>
       <div class="l-grid l-grid--3up">
-        <card
-          image="https://blacklivesmatter.com/wp-content/uploads/2020/06/blmgn-news-juneteenth-national-holiday-061720-1000x750-01-400x300.jpg"
-          altText="Make Juneteenth a National Holiday"
-          subtitle="Make Juneteenth a National Holiday"
-          :hasTextLink="true"
-          cta="Read More"
-          link="https://blacklivesmatter.com/make-juneteenth-a-national-holiday/"
-          description="Now more than ever, we must highlight and celebrate the rich history of our community and our culture. In the last few days, cities and companies across the country have declared Juneteenth a holiday. Just like our lives, our HISTORY matters, including why Juneteenth is rooted in joy and is a call for liberation for the Black community."
-        />
-        <card
-          image="https://blacklivesmatter.com/wp-content/uploads/2020/06/blmgn-news-bet-house-party-deepak-chopra-061520-1000x750-01-400x300.jpg"
-          altText="Black Lives Matter Global Network"
-          subtitle="Black Lives Matter Global Network Partners with BET House Party to Speak with Deepak Chopra, M.D."
-          :hasTextLink="true"
-          cta="Read More"
-          description="The taping of the program was completed prior to the killing of George Floyd. The original BET air date was May 29, 2020. Black Lives Matter Global Network Managing Director, Kailee Scales is joined by renowned physician, author, and philosopher, Deepak Chopra, M.D. to discuss how our minds effect our health and share ways to calm ourselves."
-        />
-        <card
-          image="https://blacklivesmatter.com/wp-content/uploads/2020/06/blmgn-news-fund-organizing-work-061120-1000x750-01-400x300.jpg"
-          altText="Black Lives Matter Global Network"
-          subtitle="Black Lives Matter Global Network Foundation Announces $6.5 Million Fund to Support Organizing Work"
-          :hasTextLink="true"
-          cta="Read More"
-          description="Black Lives Matter Global Network Foundation (BLM Global Network Foundation), a US nonprofit global organization active in the US, UK, and Canada is grateful for the generosity and support of donors and is pleased to announce a $6.5 million fund to support grassroots organizing work."
-        />
+        <template v-for="article in national_news">
+          <card
+            :key="article._uid"
+            :image="article.image.filename"
+            :altText="article.Title"
+            :subtitle="article.Title"
+            :hasTextLink="true"
+            cta="Read More"
+            :link="article.link.url"
+            :description="article.Blurb"
+          />
+        </template>
       </div>
     </section>
   </div>
@@ -109,6 +99,14 @@
       ContentCard,
       VSpacer,
       Card
+    },
+    computed: {
+      local_news () {
+        return this.story && this.story.content && this.story.content.body && this.story.content.body.filter(article => article.component === 'local-news')
+      },
+      national_news () {
+        return this.story && this.story.content && this.story.content.body && this.story.content.body.filter(article => article.component === 'national-news')
+      }
     }
   }
 </script>
