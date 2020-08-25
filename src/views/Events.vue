e212295047ba3f56f875ff32ac0cb795a31bb7ec
mari <mari@ionlearning.com>
2020-08-25 15:14:32 -0400

    events

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -4,18 +4,20 @@
     <v-spacer size="quad" class="u-hide-until--m"/>
     <v-spacer size="quad"/>
     <section class="l-container">
-      <content-card
-        title="Juneteenth March & Rally"
-        image="/img/sidebar-image.jpg"
-        layout="1x4"
-        link="https://www.instagram.com/p/CBiyVchsPUh/"
-        cta="learn more"
-        class="u-space--double--bottom"
-      >
-        <template slot="content">
-          <p>Come out and celebrate black excellence while fighting for our rights on OUR Independence Day. June 15th, Boonton 5pm.</p>
-        </template>
-      </content-card>
+      <div v-for="event in story.content.body" :key="event._uid">
+        <content-card
+          :title="event.title"
+          :image="event.image.filename"
+          layout="1x4"
+          :link="event.link.url"
+          cta="learn more"
+          class="u-space--double--bottom"
+        >
+          <template slot="content">
+            <p>{{ event.description }}</p>
+          </template>
+        </content-card>
+      </div>
     </section>
   </div>
 </template>
@@ -24,9 +26,16 @@
   import SecondaryPageHero from '../components/SecondaryPageHero'
   import VSpacer from 'vue-evolve/src/components/VSpacer'
   import ContentCard from 'vue-evolve/src/components/ContentCard'
+  import storyblok from '../mixins/storyblok.mixin'
 
   export default {
     name: 'Events',
+    mixins: [storyblok],
+    data () {
+      return {
+        slug: 'Events'
+      }
+    },
     metaInfo () {
       return {
         title: 'Black Lives Matter Morristown | Events',
