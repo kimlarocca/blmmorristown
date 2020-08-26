45447d7c119ad1b71a6c2bf1d95bff51690c4a9f
mari <mari@ionlearning.com>
2020-08-25 21:33:45 -0400

    add sidebar to actions pg

--- src/views/Actions.vue
+++ src/views/Actions.vue
@@ -1,22 +1,27 @@
 <template>
   <div>
-    <secondary-page-hero title="Events"/>
+    <secondary-page-hero title="Actions"/>
     <v-spacer size="quad" class="u-hide-until--m"/>
     <v-spacer size="quad"/>
     <section class="l-container">
-      <div v-for="event in story.content.body" :key="event._uid">
-        <content-card
-          :title="event.title"
-          :image="event.image.filename"
-          layout="1x4"
-          :link="event.link.url"
-          cta="learn more"
-          class="u-space--double--bottom"
-        >
-          <template slot="content">
-            <p>{{ event.description }}</p>
-          </template>
-        </content-card>
+      <div class="l-grid l-grid--4x1 l-grid--1up--small l-grid--large-gutters">
+        <div>
+          <div v-for="event in story.content.body" :key="event._uid">
+            <content-card
+              :title="event.title"
+              :image="event.image.filename"
+              layout="1x4"
+              :link="event.link.url"
+              cta="learn more"
+              class="u-space--double--bottom"
+            >
+              <template slot="content">
+                <p>{{ event.description }}</p>
+              </template>
+            </content-card>
+          </div>
+        </div>
+        <sidebar/>
       </div>
     </section>
   </div>
@@ -24,6 +29,7 @@
 
 <script>
   import SecondaryPageHero from '../components/SecondaryPageHero'
+  import Sidebar from '../components/Sidebar'
   import VSpacer from 'vue-evolve/src/components/VSpacer'
   import ContentCard from 'vue-evolve/src/components/ContentCard'
   import storyblok from '../mixins/storyblok.mixin'
@@ -49,6 +55,7 @@
     },
     components: {
       SecondaryPageHero,
+      Sidebar,
       VSpacer,
       ContentCard
     }
